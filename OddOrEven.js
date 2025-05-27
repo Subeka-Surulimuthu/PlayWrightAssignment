2e503c94ae9874ba7b25b3dc33e6aeeb91cd549f
Subeka-Surulimuthu <subekabe39@gmail.com>
2025-05-27 17:41:28 +0530

    Add files via upload
    25th May 2025 Odd or Even Assignment

---
+++ OddOrEven.js
@@ -0,0 +1,11 @@
+function OddorEven(number1){
+    if( number1%2===0){
+        console.log("Given number is Even")
+    }else{
+        console.log("Given number is odd");
+    }
+
+}
+
+let number=0;
+OddorEven(number)
